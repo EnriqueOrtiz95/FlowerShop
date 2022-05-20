bf68bd054ae0c78690f170c9405523c5ef580777
Enrique Ortiz <enortizlo@ittepic.edu.mx>
2022-05-19 22:39:34 -0600

    v1.1 | JS on car-icon little functionality

--- js/app.js
+++ js/app.js
@@ -1,130 +1,117 @@
-let navbar = document.querySelector('.navbar')
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarProductoBtn = document.querySelector('#vaciar-carrito');
+const products = document.querySelector('#products');
 
-document.querySelector('#menu-bar').onclick = () =>{
-    navbar.classList.toggle('active');
+// //?icono del carrito
+// const imgCarrito = document.querySelector('#img-carrito');
+
+let articulosProducto = [];
+
+cargarEventListeners();
+function cargarEventListeners(){
+    products.addEventListener('click', agregarProducto);
+
+    carrito.addEventListener('click', eliminarProducto);
+
+    vaciarProductoBtn.addEventListener('click', () => {
+        articulosProducto = [];
+        limpiarHTML(); //ELIMINAR HTML
+    })
 }
 
-document.querySelector('#close').onclick = () =>{
-    navbar.classList.remove('active');
+
+//?functions
+function agregarProducto(e){
+    e.preventDefault();
+
+    if(e.target.classList.contains('agregar-carrito')){
+        const productoSeleccionado = e.target.parentElement.parentElement.parentElement;
+        leerDatosProducto(productoSeleccionado);
+    }
 }
 
-window.onscroll = () =>{
+//?Read HTML content that was clicked and take the course's info.
+function leerDatosProducto(producto){
+    // console.log(producto);
 
-    navbar.classList.remove('active');
+    //?create an object with the content of the actual course
+    const infoProducto = {
+        imagen: producto.querySelector('picture img').src,
+        titulo: producto.querySelector('h3').textContent,
+        precio: producto.querySelector('p del').textContent,
+        id: producto.querySelector('.agregar-carrito').getAttribute('data-id'),
+        cantidad: 1
+    }
 
-    if(window.scrollY > 100){
-        document.querySelector('header').classList.add('active');
+    //*check if an element exists in the carrito
+    const existe = articulosProducto.some( producto => producto.id === infoProducto.id);
+    if(existe){
+        //?update cantidad
+        const productos = articulosProducto.map( producto => {
+            if(producto.id === infoProducto.id){
+                producto.cantidad++;
+                return producto; //TODO<< return updated course
+            }else{
+                return producto; //?<<<<< return non-updated ones
+            }
+        });
+        articulosProducto = [ ...productos];
     }else{
-        document.querySelector('header').classList.remove('active');
+        //add article to the carrito
+        articulosProducto = [ ...articulosProducto, infoProducto];
     }
 
+    // console.log(infoProducto);
+    
+    console.log(articulosProducto);
+    carritoHTML();
 }
 
-let themeToggler = document.querySelector('#theme-toggler');
+//?function eliminarProducto
+function eliminarProducto(e){
+    // console.log(e.target.classList);
+    if(e.target.classList.contains('borrar-producto')){
+        const productoId = e.target.getAttribute('data-id');
 
-themeToggler.onclick = () =>{
-    themeToggler.classList.toggle('fa-sun');
-    if(themeToggler.classList.contains('fa-sun')){
-        document.querySelector('body').classList.add('active');
-    }else{
-        document.querySelector('body').classList.remove('active');
+        //*elimina del arreglo de articuloscarrito por el data-id
+        articulosProducto = articulosProducto.filter( producto => producto.id !== productoId);
+        //console.log(articulosProducto);
+
+        carritoHTML(); //ITERAR SOBRE EL CARRITO Y MOSTRAR SU HTML
     }
 }
 
-document.querySelectorAll('.small-image-1').forEach(images =>{
-    images.onclick = () =>{
-        document.querySelector('.big-image-1').src = images.getAttribute('src');
-    }
-});
+function carritoHTML(){
 
-document.querySelectorAll('.small-image-2').forEach(images =>{
-    images.onclick = () =>{
-        document.querySelector('.big-image-2').src = images.getAttribute('src');
-    }
-});
+    //clear HTML duplicated
+    limpiarHTML();
 
-document.querySelectorAll('.small-image-3').forEach(images =>{
-    images.onclick = () =>{
-        document.querySelector('.big-image-3').src = images.getAttribute('src');
-    }
-});
 
-let countDate = new Date('aug 1, 2021 00:00:00').getTime();
-
-function countDown(){
-
-    let now = new Date().getTime();
-	gap = countDate - now;
-
-    let seconds = 1000;
-    let minutes = seconds * 60;
-    let hours = minutes * 60;
-    let days = hours * 24;
-
-    let d = Math.floor(gap / (days));
-	let h = Math.floor((gap % (days)) / (hours));
-	let m = Math.floor((gap % (hours)) / (minutes));
-	let s = Math.floor((gap % (minutes)) / (seconds));
-
-    document.getElementById('days').innerText = d;
-    document.getElementById('hours').innerText = h;
-    document.getElementById('minutes').innerText = m;
-    document.getElementById('seconds').innerText = s;
-
+    //arrange carrito and generate HTML
+    articulosProducto.forEach( producto => {
+        const { imagen, titulo, precio, cantidad, id} = producto;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td> <img src="${imagen}" width=100> </td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href="#" class="borrar-producto" data-id="${id}">X</a>
+            </td>
+        `;
+        //ADD HTML from carrito in the tbody
+        contenedorCarrito.appendChild(row);
+    })
 }
 
-setInterval(function(){
-    countDown()
-},1000);
+//eliminar productos del tbody
+function limpiarHTML(){
+    // slow way
+    // contenedorCarrito.innerHTML = "";
 
-var swiper = new Swiper(".product-slider", {
-    slidesPerView: 3,
-    loop:true,
-    spaceBetween: 10,
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        550: {
-          slidesPerView: 2,
-        },
-        800: {
-          slidesPerView: 3,
-        },
-        1000: {
-            slidesPerView: 3,
-        },
-    },
-});
-
-var swiper = new Swiper(".review-slider", {
-    slidesPerView: 3,
-    loop:true,
-    spaceBetween: 10,
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        550: {
-          slidesPerView: 2,
-        },
-        800: {
-          slidesPerView: 3,
-        },
-        1000: {
-            slidesPerView: 3,
-        },
-    },
-});
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+}
